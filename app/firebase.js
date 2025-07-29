8006f9c4f182cfbc9759a91a5c13115f9cbcaefc
Ngonidzashe Mwelo <profn446@gmail.com>
2025-07-30 01:06:03 +0200

    Commiting the build

--- app/firebase.js
+++ app/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBOkX8-mNS9YjeHZAziIRPb26aOJXgDgzU",
   authDomain: "checklist-859b4.firebaseapp.com",
   projectId: "checklist-859b4",
-  storageBucket: "checklist-859b4.firebasestorage.app",
+  storageBucket: "checklist-859b4.appspot.com",
   messagingSenderId: "939429794024",
   appId: "1:939429794024:web:a68bb2e27e39b7d747795d",
-  measurementId: "G-WRP1VS9RKB"
+  measurementId: "G-WRP1VS9RKB",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
